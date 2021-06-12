ab58cd8faf09100bfb4d9d9eb665ebd801c69e9a
u-kai <76635578+u-kai@users.noreply.github.com>
2021-06-12 19:16:43 +0900

    create UpdateMakre

--- src/model/UpdateMaker.ts
+++ src/model/UpdateMaker.ts
@@ -1,13 +1,26 @@
 import { UpdateInfo } from 'type/UpdateInfo'
-
+import {WhereClauseElements} from "type/WhereClauseElements"
+import {WhereClauseMaker} from "model/WhereClauseMaker"
 export class UpdateMaker {
     private headSQL: string
     private updateInfo: UpdateInfo
+    private whereClauseElements:WhereClauseElements
     constructor(updateInfo: UpdateInfo) {
         this.headSQL = 'UPDATE'
         this.updateInfo = updateInfo
+        this.whereClauseElements = updateInfo.whereClauseElements
+    }
+    createWhereClase = ():string => {
+        const whereClauseMaker = new WhereClauseMaker(this.whereClauseElements)
+        return whereClauseMaker.createWhereClause()
+    }
+    private createkeyValues = ():string => {
+        const keyValues = this.updateInfo.updateKeys.map((key,i)=>{
+            return `${key} = '${this.updateInfo.updateValues[i]}'`
+        })
+        return keyValues.join(",")
     }
     outputSQL = () => {
-        console.log()
+        return `${this.headSQL} ${this.updateInfo.tableName} SET ${this.createkeyValues()} ${this.createWhereClase()}`
     }
 }
--- src/tests/model/UpdateMaker.test.ts
+++ src/tests/model/UpdateMaker.test.ts
@@ -12,5 +12,5 @@ const updateMaker = new UpdateMaker(updateInfo)
 const sql = updateMaker.outputSQL()
 console.log(sql)
 it('test outputSQL is ok?', () => {
-    expect(sql).toBe("UPDATE test SET user = 'u-kai', age = '18' WHERE user = `kai` AND name = `udo`")
+    expect(sql).toBe("UPDATE test SET user = 'u-kai',age = '18' WHERE user = 'kai' AND name = 'udo'")
 })
