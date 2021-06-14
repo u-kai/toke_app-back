521c847c2b156018fb0268316e9701a1ef515322
u-kai <76635578+u-kai@users.noreply.github.com>
2021-06-14 15:51:14 +0900

    create sqlInfoMaker

---
+++ src/model/SQLInfoMaker.ts
@@ -0,0 +1,52 @@
+import {WhereOperator} from "types/DB-types/WhereOperator"
+import {SelectInfo} from "types/DB-types/SelectInfo"
+import { InsertInfo } from "types/DB-types/InsertInfo"
+import { UpdateInfo } from "types/DB-types/UpdateInfo"
+
+export class SQLInfoMaker{
+    tableName:string
+    constructor(tableName:string){
+        this.tableName = tableName
+    }
+    makeSelectInfo = (
+        selectDatas: string[],
+        whereKeys: string[],
+        whereValues: string[],
+        whereOperators: WhereOperator[],
+    ): SelectInfo => {
+        return {
+            selectDatas: selectDatas,
+            tableName: this.tableName,
+            whereClaseElements: {
+                whereKeys: whereKeys,
+                whereValues: whereValues,
+                whereOperators: whereOperators,
+            },
+        }
+    }
+    makeInsertInfo = (insertKeys: string[],insertValues: string[],whereKeys:string[],whereValues:string[],whereOperators:WhereOperator[]):InsertInfo => {
+        return {
+            tableName:this.tableName,
+            insertKeys:insertKeys,
+            insertValues:insertValues,
+            whereClauseElements:{
+                whereKeys:whereKeys,
+                whereValues:whereValues,
+                whereOperators:whereOperators
+            }
+        }
+    }
+    makeUpdateInfo = (updateKeys:string[],updateValues:string[],whereKeys:string[],whereValues:string[],whereOperators:WhereOperator[]):UpdateInfo => {
+        return {
+            tableName:this.tableName,
+            updateKeys:updateKeys,
+            updateValues:updateValues,
+            whereClauseElements:{
+                whereKeys:whereKeys,
+                whereValues:whereValues,
+                whereOperators:whereOperators
+            }
+        }
+    }
+    // makeDeleteInfo = ():DeleteInfo=>{}
+}
